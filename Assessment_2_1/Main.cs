2e4d608faee2ffaa1c9e386a7236cfde87ab75f4
Grace <54424892+Tawncat@users.noreply.github.com>
2019-10-07 10:52:21 +1100

    Assessment2
    Added assessment 2 folder.
    Added assessment 2 program 1 - 2D graphics class.

---
+++ Assessment_2_1/Main.cs
@@ -0,0 +1,38 @@
+﻿//****************************************************************
+// Programmer: Grace Toyer CIT014747
+// Date: 07/10/2019
+// Software: Microsoft Visual Studio 2019 Community Edition
+// Platform: Microsoft Windows 10 Professional 64-bit
+// Purpose: Assessment 2 - A 2D graphic created using the C# Graphics class.
+//******************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assessment_2_1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDraw_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
