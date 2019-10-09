931c96503422d2b592da4064a075e307e78a1f49
Grace <54424892+Tawncat@users.noreply.github.com>
2019-10-09 18:20:07 +1100

    Program 5 and 7
    Added completed assessment items ; 5(nested classes) and 7(C++ program).

---
+++ Assessment_2/Assessment_2_7/Main.cpp
@@ -0,0 +1,37 @@
+//****************************************************************
+// Programmer: Grace Toyer CIT014747
+// Date: 09/10/2019
+// Software: Microsoft Visual Studio 2019 Community Edition
+// Platform: Microsoft Windows 10 Professional 64-bit
+// Purpose: Assessment 2 - A program that subtracts 2 numbers and outputs the result. Same as Assessment 1A.
+// Language: C++
+//******************************************************************
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double num1;
+	double num2;
+	double answer;
+
+	cout << "Enter 2 decimal numbers with a space or enter inbetween:\n";
+	while (1)//check if a deciaml number is entered
+	{
+		cin >> num1 >> num2;
+		if (cin.good())
+		{
+			break;
+		}
+		else {
+			cout << "Input 2 decimal numbers only.\n";
+			cin.clear();
+			cin.ignore(100000, '\n');
+		}
+	}
+
+	answer = num1 - num2;
+
+	cout << num1 << "-" << num2 << " is " << answer;
+}
